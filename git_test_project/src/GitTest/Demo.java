4ec977e2ee08adba53d13e34b01a6ab20053e3b0
xuchen <460418637@qq.com>
2021-02-15 19:59:34 +0800

    class committed over again

--- git_test_project/src/GitTest/Demo.java
+++ git_test_project/src/GitTest/Demo.java
@@ -3,5 +3,9 @@ package GitTest;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("hello github i am coming!!!");
+        System.out.println("This has been modified!!!!!!!!!!!!!!!!!!");
+        System.out.println("This has been modified!!!!!!!!!!!!!!!!!!");
+        System.out.println("This has been modified!!!!!!!!!!!!!!!!!!");
+        System.out.println("This has been modified!!!!!!!!!!!!!!!!!!");
     }
 }
